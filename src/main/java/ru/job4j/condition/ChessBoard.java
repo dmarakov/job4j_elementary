483808448c36ad4ca5d0a311c6574b1f8ed2bcb0
Dmitry Marakov <dmarakov@devexperts.com>
2022-12-20 21:59:06 +0400

    Fixed else options for ChessBoard

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -6,8 +6,6 @@ public class ChessBoard {
         if (isValid(x1) && isValid(y1) && isValid(x2) && isValid(y2)) {
             if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                 rsl = Math.abs(x1 - x2);
-            } else {
-                rsl = 0;
             }
         }
         return rsl;
