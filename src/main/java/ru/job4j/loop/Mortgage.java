0aa1f822bfbace64e09753e4e9006d2473ae6113
Dmitry Marakov <dmarakov@devexperts.com>
2022-12-25 21:41:48 +0400

    Refactoring Mortgage logic

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,9 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
-        int year = 1;
+        int year = 0;
         percent /= 100;
-        while (amount + (amount * percent) > salary) {
+        while (amount > 0) {
             amount = amount + (amount * percent) - salary;
             year++;
         }
