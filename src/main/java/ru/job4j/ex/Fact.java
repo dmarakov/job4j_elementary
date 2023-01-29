781e59e83c2bea2e9ca243ff99be302b6e168079
Dmitry Marakov <dmarakov@devexperts.com>
2023-01-29 15:58:54 +0400

    Fixed rsl variable declaration place

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -2,10 +2,10 @@ package ru.job4j.ex;
 
 public class Fact {
     public int calc(int n) {
-        int rsl = 1;
         if (n < 0) {
             throw new IllegalArgumentException("N could not be less then ");
         }
+        int rsl = 1;
         for (int index = 2; index <= n; index++) {
             rsl *= index;
         }
